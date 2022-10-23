b3d8965e7be15bc0ad90b62ab3b00a178a62337e
TechngounLeang <116193325+TechngounLeang@users.noreply.github.com>
2022-10-23 22:26:17 +0700

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,10 +1,10 @@
-number = [4,2,3,1]
-def replace_last(number):
-    if len(number) <= 1:
-        return number
+array = [4,2,3,1]
+def replace_last(list_1):
+    if len(list_1) <= 1:
+        return list_1
     else:
-        number.insert(0, number.pop())
-        return number
+        list_1.insert(0, list_1.pop())
+        return list_1
 
-new_number = replace_last(number)
-print (new_number)
+new_list = replace_last(array)
+print (new_list)
