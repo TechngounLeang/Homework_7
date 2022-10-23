7411a35d03f85d5cfefad995b658a03930eb1e86
TechngounLeang <116193325+TechngounLeang@users.noreply.github.com>
2022-10-23 22:40:45 +0700

    Update exercise-5.py

--- exercise-5.py
+++ exercise-5.py
@@ -3,5 +3,5 @@ def backword_string_by_word(Sentence):
     
     return ' '.join(word[::-1] for word in Sentence.split(" "))
  
-Sentence = "'Hello World"
+Sentence = input("Enter a string: ")
 print(backword_string_by_word(Sentence))  
