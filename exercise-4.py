04f72aeea9ee04d1a3fd80f061afa14922464d58
TechngounLeang <116193325+TechngounLeang@users.noreply.github.com>
2022-10-23 23:14:45 +0700

    Update exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,14 +1,14 @@
-array = [1, 2, 3, 4, 5, 6]
-N = 2
+list_1 = [1, 2, 3, 4, 5, 6]
+chunk_size = 2
 
-def chunking_by(array, N):
+def chunking_by(array, size):
     if len(array) == 0:
         return array
     else:
-        chunk = list()
-        for i in range (0, len(array), N):
-            chunk.append(array[i: i+N])
-        return chunk
+        chunked = list()
+        for i in range (0, len(array), size):
+            chunked.append(array[i: i+size])
+        return chunked
 
-new = chunking_by(array, N)
-print(new)
+new_list = chunking_by(list_1, chunk_size)
+print(new_list)
